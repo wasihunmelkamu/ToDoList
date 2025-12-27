e5f553cfbec2f8401ea3f09eedea2b4cb7372069
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-27 01:19:16 -0900

    build auth ts

---
+++ auth.ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+
+      clientId:process.env.AUTH_GOOGLE_ID,
+      clientSecret:process.env.AUTH_GOOGLE_SECRET
+
+    }),
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "wasman@gmail.com",
+        },
+        password: { label: "Password", type: "password", placeholder: "*****" },
+      },
+      authorize: async (credentials) => {
+        if (!credentials?.email || !credentials?.password) return;
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+        if (!user || !user.password) return;
+        if (!user.emailVerified) {
+          throw new Error("Email not verified");
+        }
+        const isValid = await bcrypt.compare(
+          credentials.password as string,
+          user.password as string
+        );
+        if (!isValid) return null;
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          image: user.image,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  session: { strategy: "database" },
+});
