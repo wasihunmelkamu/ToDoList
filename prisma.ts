1a179102a08a18c4d8a8d961c9e50baa813e52e9
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-27 01:19:57 -0900

    set one  globale prisma instance and add pooling

---
+++ prisma.ts
@@ -0,0 +1,10 @@
+
+import { PrismaClient } from "./lib/generated/prisma"
+import { withAccelerate } from "@prisma/extension-accelerate"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
